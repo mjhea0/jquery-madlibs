f8afa778045e5be87b14992d3379b8d62f04a23c
Michael Herman <hermanmu@gmail.com>
2013-12-12 23:54:21 -0700

    initial

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$(function() {
+  $("form").submit(function(e) {
+    var str = $("form").serialize();
+    console.log(str);
+    e.preventDefault()
+    $(".person").empty().append($("input.person").val());
+    $(".adjective").empty().append($("input.adjective").val());
+    $(".noun").empty().append($("input.noun").val());
+    $(".insect").empty().append($("input.insect").val());
+    $(".noun2").empty().append($("input.plural-noun").val());
+    $(".verb").empty().append($("input.verb").val());
+    $("#story").show();
+    $(':input').val('');
+    $("#questions").hide();
+  });
+  $("#play-btn").click(function(e) {
+  	e.preventDefault()
+    $("#questions").show(); 	
+  	$("#story").hide();
+  });
+});
