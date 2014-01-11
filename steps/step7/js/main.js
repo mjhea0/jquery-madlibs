5f9e95f055ddf935ec8af0fbeaefb7588d4aec11
Michael Herman <hermanmu@gmail.com>
2014-01-11 04:24:46 -0700

    step7

---
+++ steps/step7/js/main.js
@@ -0,0 +1,28 @@
+$(function() {
+
+  // hide the story from view
+  $("#story").hide();
+
+  // ---- event handler ---- //
+  $("#btn-click").click(function(e) {
+
+    // grab the values from the input boxes, then append them to the DOM
+    $(".person").empty().append($("input.person").val());
+    $(".adjective").empty().append($("input.adjective").val());
+    $(".noun").empty().append($("input.noun").val());
+    $(".insect").empty().append($("input.insect").val());
+    $(".noun2").empty().append($("input.plural-noun").val());
+    $(".verb").empty().append($("input.verb").val());
+
+    // show the story
+    $("#story").show();
+
+    // empty the form's values
+    $(':input').val('');
+
+    // hide the questions
+    $("#questions").hide();
+
+  });
+
+});
