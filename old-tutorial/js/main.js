cd7aee2513227585ac90b5c13bb7db4191746bc5
Michael Herman <hermanmu@gmail.com>
2014-01-11 04:34:22 -0700

    updates

--- old-tutorial/js/main.js
+++ old-tutorial/js/main.js
@@ -1,34 +1,21 @@
 $(function() {
-
-  // hide the story from view
-  $("#story").hide();
-
-  // ---- event handler ---- //
-  $("#btn-click").click(function(e) {
-
-    // grab the values from the input boxes, then append them to the DOM
+  $("form").submit(function(e) {
+    var str = $("form").serialize();
+    console.log(str);
+    e.preventDefault()
     $(".person").empty().append($("input.person").val());
     $(".adjective").empty().append($("input.adjective").val());
     $(".noun").empty().append($("input.noun").val());
     $(".insect").empty().append($("input.insect").val());
     $(".noun2").empty().append($("input.plural-noun").val());
     $(".verb").empty().append($("input.verb").val());
-
-    // show the story
     $("#story").show();
-
-    // empty the form's values
     $(':input').val('');
-
-    // hide the questions
     $("#questions").hide();
-
   });
-
-  // ---- event handler ---- //
   $("#play-btn").click(function(e) {
-    $("#questions").show();   
-    $("#story").hide();
+  	e.preventDefault()
+    $("#questions").show(); 	
+  	$("#story").hide();
   });
-
 });
